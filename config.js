81b64df8099c6eb665b7d2df989c53181c57edbf
absoluteDumbass <161618159+absoluteDumbass@users.noreply.github.com>
2025-10-03 13:52:29 +0800

    Create config.js
    qol for me

---
+++ config.js
@@ -0,0 +1,23 @@
+const data = {
+  // key: { cost: [manpower, gold], name: "name" }
+  line_infantry: { cost: [200, 50], name: "Line Infantry" },
+  light_infantry: { cost: [225, 75], name: "Light Infantry" },
+  guard: { cost: [275, 75], name: "Guards" },
+  grenadier: { cost: [250, 50], name: "Grenadiers" },
+  militia: { cost: [125, 50], name: "Militia" },
+  rifles: { cost: [50, 25], name: "Rifles" },
+  hussar: { cost: [100, 50], name: "Hussars" },
+  lancer: { cost: [125, 75], name: "Lancers" },
+  dragoon: { cost: [125, 50], name: "Dragoons" },
+  cuirassier: { cost: [150, 75], name: "Cuirassiers" },
+  horse_archer: { cost: [75, 50], name: "Horse Archers" },
+  "18lb li": { cost: [50, 175], name: "18-lb Licorne" },
+  "12lb": { cost: [50, 200], name: "12-lb Foot Artillery" },
+  "10lb li": { cost: [50, 150], name: "10-lb Licorne" },
+  "8lb": { cost: [25, 150], name: "8-lb Foot Artillery" },
+  "6lb": { cost: [25, 125], name: "6-lb Foot Artillery" },
+  "4lb": { cost: [25, 100], name: "4-lb Foot Artillery" },
+  howitzer: { cost: [50, 100], name: "6-in Howitzers" },
+  horse_artillery: { cost: [75, 125], name: "4-lb Horse Artillery" },
+  rockets: { cost: [25, 200], name: "Rockets" }
+};
